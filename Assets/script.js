68fd0e6dc8509c9e3001002f177e3aa2571b5a53
momofastorm <marcyrappaport.blessed@gmail.com>
2023-04-18 17:10:24 -0500

    everything working final push

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,58 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
 
-  //Get current day
- var today = dayjs().format('[MMM DD, YYYY}');
- console.log(today);
+//Get current day
+var today = dayjs().format('MMMM DD, YYYY hh:mm a');
+let store;
 
- $("currentDay").text(today);
- var currentHour = moment().format('HH')
+const getStorage = async () => {
+  store = localStorage.hours ? JSON.parse(localStorage.hours) : [];
+  store.forEach((task,i) => {
+    textareas[i].value = task
+  });
+}
+
+$("#currentDay").text(today);
+var currentHour = dayjs().format('HH')
+
+const hours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+
+hours.forEach((hour,i) => {
+  let rowHour = i+9;
+  main.innerHTML+=`
+  <div class="row time-block ${rowHour<currentHour ? 'past' : rowHour>currentHour ? 'future' : 'present'}">
+  <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
+  <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+    <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
+</div>
+  `;
+});
+
+const textareas = document.querySelectorAll('textarea');
+getStorage();
  
  //listener for click events on the save button.
  $(".saveBtn").on("click", function () {
+  store = [];
+
+    textareas.forEach(area => {
+      store.push(area.value);    
+    });
+
+    localStorage.hours = JSON.stringify(store);
  });
+ 
+
+
+
   // TODO:   
   // This code should use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // function? 
+ // How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
@@ -31,4 +67,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
